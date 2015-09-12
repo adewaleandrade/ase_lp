84ae6a41e53c0ecb8f53e3bc58523075925206f9
Adewale  Andrade <adewale.andrade@gmail.com>
2015-09-11 22:43:18 -0300

    firstcommit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>Ache Seu Studio</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/starter-template.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="js/ie-emulation-modes-warning.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <style>
+        body {
+            background: white }
+        section {
+            background: white;
+            color: #323031;
+            border-radius: 1em;
+            padding: 1em;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%) }
+      </style>
+  </body>
+        <section>
+            <img src="img/logo.jpg">
+            <h1>Em Breve!</h1>
+            <p>Receba novidades!</p>
+            <!-- <a class="button btn-success" href="#" >Cadastro</a> -->
+            <a type="button" class="btn btn-lg btn-primary" href="https://docs.google.com/forms/d/1iVH5hrVy9FCq2qeiMV_hvhwYg8HwbgUm8EnfKRqo05E/viewform?usp=send_form">Cadastro</a>
+        </section>
+  </body>
+</html>
